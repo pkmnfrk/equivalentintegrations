a8ca87d6c2f04d14513f96f59130b49b4fe83dc7
Mike Caron <caron.mike@gmail.com>
2019-02-13 14:52:48 -0500

    remove the TOP and WAILA integrations, since the base library will handle that for me (I can't remove the dependencies, however, since I use them)

--- src/main/java/com/mike_caron/equivalentintegrations/integrations/MainCompatHandler.java
+++ src/main/java/com/mike_caron/equivalentintegrations/integrations/MainCompatHandler.java
@@ -1,28 +1,8 @@
 package com.mike_caron.equivalentintegrations.integrations;
 
-import net.minecraftforge.fml.common.Loader;
-
 public class MainCompatHandler
 {
     public static void registerAll()
     {
-        registerTOP();
-        registerWaila();
-    }
-
-    public static void registerTOP()
-    {
-        if(Loader.isModLoaded("theoneprobe"))
-        {
-            TOPCompatibility.register();
-        }
-    }
-
-    public static void registerWaila()
-    {
-        if(Loader.isModLoaded("waila"))
-        {
-            WailaCompatibility.register();
-        }
     }
 }
