0ae408ac753ba4e1c50b277d4bf8ecb426b4c7a5
Mike Caron <caron.mike@gmail.com>
2019-02-13 16:21:10 -0500

    update source mappings, update an error caused by that, and bump the version

--- src/main/java/com/mike_caron/equivalentintegrations/CreativeTab.java
+++ src/main/java/com/mike_caron/equivalentintegrations/CreativeTab.java
@@ -12,7 +12,7 @@ public class CreativeTab extends CreativeTabs
     }
 
     @Override
-    public ItemStack getTabIconItem()
+    public ItemStack createIcon()
     {
         return new ItemStack(ModItems.efficiencyCatalyst, 1);
     }
